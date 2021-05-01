aadeabf8c6bd8deb651dde3cf1b117b666ab0274
mcbba <77134855+mcbba@users.noreply.github.com>
2021-04-30 21:05:15 -0700

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
 // var googleDocURL = 'https://docs.google.com/spreadsheets/d/1iuiz5n0cJLFOvzn8KMBYx4Qsa9Te5KJc8O-ZqNWGm4s/edit#gid=0';
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1xiVsJyZgNXziuB4ZcCw-_Pww6gfLpF3wsrYWIh6dLp4/edit?usp=sharing';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1xiVsJyZgNXziuB4ZcCw-_Pww6gfLpF3wsrYWIh6dLp4/edit#gid=0';
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyCq3m304-zo15BPsOtlR3BnHsClkaCYBxg';
