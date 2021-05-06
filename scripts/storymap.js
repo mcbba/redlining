3705e5d145b8239ac2413613654b9b377b8e88db
mcbba <77134855+mcbba@users.noreply.github.com>
2021-05-06 08:43:22 -0700

    Update storymap.js

--- scripts/storymap.js
+++ scripts/storymap.js
@@ -469,7 +469,7 @@ $(window).on('load', function() {
    */
   function changeAttribution() {
     var attributionHTML = $('.leaflet-control-attribution')[0].innerHTML;
-    var credit = '&nbsp &nbsp &nbsp &nbsp';
+    var credit = 'Made for MCBBA';
 
       var name = getSetting('_authorName');
     var url = getSetting('_authorURL');
@@ -486,7 +486,7 @@ $(window).on('load', function() {
     credit += ' ';
     if (getSetting('_codeCredit')) credit += ' by ' + getSetting('_codeCredit');
     credit += ' with ';
-    $('.leaflet-control-attribution')[0].innerHTML = credit; //+ attributionHTML;
+    $('.leaflet-control-attribution')[0].innerHTML = credit + '<a href="https://leafletjs.com/" target="_blank">Leaflet</a>';
   }
 
 });
