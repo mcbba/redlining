605e9c86a59f9fbc1412ebdfb2284d054c91f7d9
mcbba <77134855+mcbba@users.noreply.github.com>
2021-08-27 17:52:45 -0600

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,6 +1,3 @@
-// paste in your published Google Sheets URL from the browser address bar
-// var googleDocURL = 'https://docs.google.com/spreadsheets/d/1iuiz5n0cJLFOvzn8KMBYx4Qsa9Te5KJc8O-ZqNWGm4s/edit#gid=0';
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1xiVsJyZgNXziuB4ZcCw-_Pww6gfLpF3wsrYWIh6dLp4/edit#gid=0';
-// insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyCq3m304-zo15BPsOtlR3BnHsClkaCYBxg';
 // new key for 2022: AIzaSyAJ2ThY0Xqtjl4MXohW-AhgqwfrctgT5zw
