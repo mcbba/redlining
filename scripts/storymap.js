f5958aa0bb1f2dd5cae4332e7b34507ce55830e3
wwiskes <83665599+wwiskes@users.noreply.github.com>
2023-04-01 12:38:35 -0600

    Update storymap.js

--- scripts/storymap.js
+++ scripts/storymap.js
@@ -84,10 +84,12 @@ $(window).on('load', function() {
    * Loads the basemap and adds it to the map
    */
  
-	L.tileLayer('https://{s}.tile.thunderforest.com/pioneer/{z}/{x}/{y}.png?apikey={apikey}', {
-		attribution: '&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-		apikey: 'db5ae1f5778a448ca662554581f283c5',
-		maxZoom: 22
+	L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
+		attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+		subdomains: 'abcd',
+		minZoom: 1,
+		maxZoom: 16,
+		ext: 'jpg'
 	}).addTo(map);
 
 	L.tileLayer('tiles/{z}/{x}/{y}.png', {
